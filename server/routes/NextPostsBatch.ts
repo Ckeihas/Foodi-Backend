0541108c1ab81316cff4ec11227ad4730d168803
Christian Keihäs <christiankeihas@Christian-MacBook-Pro.local>
2024-02-22 15:46:47 +0200

    Pagination update

--- server/routes/NextPostsBatch.ts
+++ server/routes/NextPostsBatch.ts
@@ -20,10 +20,11 @@ interface IUserPosts {
 type DocumentReference = admin.firestore.DocumentReference;
 type QuerySnapshot = admin.firestore.QueryDocumentSnapshot;
 
-router.post('/posts', verifyAccessToken, async (req: express.Request, res: express.Response) => {
+router.post('/posts/next-page', verifyAccessToken, async (req: express.Request, res: express.Response) => {
     const loggedInUser: string | undefined = req.userId;
     const getLastVisibleItem = req.body.data;
     
+    console.log("get last visible frontend: ", getLastVisibleItem)
     const userRef = db.collection('users').doc(loggedInUser);
     const userDoc = await userRef.get();
 
@@ -31,19 +32,26 @@ router.post('/posts', verifyAccessToken, async (req: express.Request, res: expre
     let lastVisible = ""
 
     try {
-        if(getLastVisibleItem == undefined){
-            const getPosts = await userDoc.data().friends.map(async (friendId: DocumentReference) => {
-                const postsRef = db.collection('posts')
-                const findPosts = await postsRef.where('userId', '==', friendId.id).orderBy('timestamp').limit(2).get();
+        const getPosts = await userDoc.data().friends.map(async (friendId: DocumentReference) => {
+            
+            const postsRef = db.collection('posts')
+            const lastVisibleItemSnapshot = db.doc(getLastVisibleItem);
+            const getDoc = await lastVisibleItemSnapshot.get();
+           
+            const findPosts = await postsRef
+            .where('userId', '==', friendId.id)
+            .orderBy('timestamp')
+            .startAfter(getDoc)
+            .limit(2)
+            .get();
+
+            if(!findPosts.empty){
+                console.log("findpost found")
                 const getLastVisible = findPosts.docs[findPosts.docs.length - 1];
 
-                if (getLastVisible) {
-                    lastVisible = getLastVisible.ref.path;
-                } else {
-                    console.log("No documents found.");
-                }
+                lastVisible = getLastVisible.ref.path; 
+                
                 findPosts.forEach((doc: QuerySnapshot) => {
-                    
                     usersPosts.push({
                         imageURL: doc.data().imageURL,
                         analyzedInstructions: doc.data().analyzedInstructions,
@@ -56,26 +64,27 @@ router.post('/posts', verifyAccessToken, async (req: express.Request, res: expre
                         likes: doc.data().likes
                     })
                 });
-            })
-            await Promise.all(getPosts).then(() => {
-                console.log("last visible: ", lastVisible)
-                if(!lastVisible){
-                    res.json({
-                        error: false,
-                        endReached: true,
-                        message: "You have reached to end"
-                    })
-                } else {
-                    res.json({
-                        error: false,
-                        endReached: false,
-                        userFriendsPosts: usersPosts,
-                        lastVisibleItem: lastVisible
-                    })
-                }
-            })
-        }
-        
+            } else {
+                console.log("No documents found.");
+            }   
+        })
+        await Promise.all(getPosts).then(() => {
+            console.log("last visible: ", lastVisible)
+            if(!lastVisible){
+                res.json({
+                    error: false,
+                    endReached: true,
+                    message: "You have reached to end"
+                })
+            } else {
+                res.json({
+                    error: false,
+                    endReached: false,
+                    userFriendsPosts: usersPosts,
+                    lastVisibleItem: lastVisible
+                })
+            }
+        })
     } catch (error) {
         console.log("Error: ", error)
         res.json({
